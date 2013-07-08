a3539ca3aa005eeebcf79d89b5a8a4fbe2ba8d3b
zeloat <zeloat1203@gmail.com>
2013-07-08 18:31:16 +0800

    Callbacks

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,10 +1,9 @@
-// String to Object options format cache
 // 用于缓存 options
 var optionsCache = {};
 
-// Convert String-formatted options into Object-formatted ones and store in cache
-// 函数作用: createOptions( 'test1 test2' );
-// { test1: true, test2: true }; 
+// 函数作用
+// createOptions( 'test1 test2' );
+// 返回值 { test1: true, test2: true }; 
 function createOptions( options ) {
 	// 在 optionsCache 中创建一个名为 options 的空对象
 	var object = optionsCache[ options ] = {};
@@ -40,6 +39,11 @@ function createOptions( options ) {
  *	stopOnFalse:	interrupt callings when a callback returns false
  *
  */
+ // Callbacks 利用了闭包原理，在 function 定义了一些内部参数，
+ // 如状态标志、函数数组、fire操作。返回了一个对外的 Object，里面
+ // 包含需要用到的函数。个人觉得这样并不是最好的写法，因为每次执行 Callbacks() 都要创建
+ // 一个包含有 add、remove 等等函数的新对象，相对于 prototype 创建对象
+ // 的方式理论上性能差一些
 jQuery.Callbacks = function( options ) {
 
 	// Convert options from String-formatted to Object-formatted if needed
@@ -62,29 +66,47 @@ jQuery.Callbacks = function( options ) {
 		// End of the loop when firing
 		firingLength,
 		// Index of currently firing callback (modified by remove if needed)
+		// 正在 fire 的函数下标
 		firingIndex,
 		// First callback to fire (used internally by add and fireWith)
 		firingStart,
 		// Actual callback list
+		// 存放函数的 list
 		list = [],
 		// Stack of fire calls for repeatable lists
+		// 参数栈，如果正在 firing 函数，而且函数内也调用了 fire 函数，就把
+		// 函数放进栈里，等待上一个参数执行完以后再依次执行栈内参数。
+		// 如果设置了 once，就把 stack 设置为 false
 		stack = !options.once && [],
 		// Fire callbacks
+		// fire 函数
 		fire = function( data ) {
+			// 如果 options 设置了 memory，就把 data 的值放到 memory 中
 			memory = options.memory && data;
+			// 设置 fired 为 true
 			fired = true;
+			// 设置 fireIndex
 			firingIndex = firingStart || 0;
+			// 设置开始
 			firingStart = 0;
+			// 记录列表长度
 			firingLength = list.length;
+			// 标记正在执行函数
 			firing = true;
+			// 执行 list 里的函数
 			for ( ; list && firingIndex < firingLength; firingIndex++ ) {
+				// 这个函数的 data 并不是 $.Callback().fire( DATA ) 传进的参数，而是通过
+				// 下面的 fireWith 传进来的值。如果设置了了 stopOnFalse，就不再继续执行后面的函数。
 				if ( list[ firingIndex ].apply( data[ 0 ], data[ 1 ] ) === false && options.stopOnFalse ) {
 					memory = false; // To prevent further calls using add
 					break;
 				}
 			}
+			// 把执行标记重新设置为 false
 			firing = false;
 			if ( list ) {
+				// 默认都是走一步的，但是如果 lock、disable、once、memory 等操作，会把
+				// stack 设置为非值，会走下面的步骤
 				if ( stack ) {
 					if ( stack.length ) {
 						fire( stack.shift() );
